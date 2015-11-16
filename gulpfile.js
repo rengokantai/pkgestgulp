91c9cb6bc402c084f625a7e0ae64d8e2504ebad4
Hernan Y.Ke <yuriqiao@gmail.com>
2015-11-15 22:55:16 -0500

    basic commands and tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,31 @@
 /**
  * Created by Hernan Y.Ke on 2015/11/17.
  */
-var gulp= require("gulp");
-var concat= require("gulp-concat");
-var uglify= require("gulp-uglify");
-var myth= require("gulp-myth");
-
-gulp.task('styles',function(){
+var gulp = require("gulp");
+var concat = require("gulp-concat");
+var uglify = require("gulp-uglify");
+var jshint = require("gulp-jshint");
+var myth = require("gulp-myth");
+var imagemin = require("gulp-imagemin");
+gulp.task('styles', function () {
     return gulp.src('app/css/*.css')
         .pipe(concat('all.css')).pipe(myth()).pipe(gulp.dest('dist/'));
 });
 
 
-gulp.task('scripts',function(){
-    return gulp.src('app/js/*.js').pipe(uglify('all.js')).pipe(uglify()).pipe(gulp.dest('dist/'))
+gulp.task('scripts', function () {
+    return gulp.src('app/js/*.js').pipe(jshint()).pipe(jshint.reporter('default')).pipe(uglify('all.js')).pipe(uglify()).pipe(gulp.dest('dist/'))
 })
 
-gulp.task('watch',function(){
-    gulp.watch('app/css/*.css',styles);
-    gulp.watch('app/js/*.js',scripts);
+gulp.task('images',function(){
+    return gulp.src('app/img/*').pipe(imagemin()).pipe(gulp.dest('dist/img'))
+})
+
+gulp.task('watch', function () {
+    gulp.watch('app/css/*.css', 'styles');
+    gulp.watch('app/js/*.js', 'scripts');
+    gulp.watch('app/img/*', 'images');
 })
 
 
-gulp.task('default',gulp.parallel('watch','scripts','styles'));
+gulp.task('default', gulp.parallel('images', 'scripts', 'styles','watch'));
