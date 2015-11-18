6959a1f87462e19f5dfc6eafaa57d781ddf6d5fe
Hernan Y.Ke <yuriqiao@gmail.com>
2015-11-17 23:28:46 -0500

    connect, static-serve

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ var uglify = require("gulp-uglify");
 var jshint = require("gulp-jshint");
 var myth = require("gulp-myth");
 var imagemin = require("gulp-imagemin");
+var connect = require("connect");
+var serve = require("serve-static");
 gulp.task('styles', function () {
     return gulp.src('app/css/*.css')
         .pipe(concat('all.css')).pipe(myth()).pipe(gulp.dest('dist/'));
@@ -15,17 +17,24 @@ gulp.task('styles', function () {
 
 gulp.task('scripts', function () {
     return gulp.src('app/js/*.js').pipe(jshint()).pipe(jshint.reporter('default')).pipe(uglify('all.js')).pipe(uglify()).pipe(gulp.dest('dist/'))
-})
+});
 
 gulp.task('images',function(){
     return gulp.src('app/img/*').pipe(imagemin()).pipe(gulp.dest('dist/img'))
-})
+});
+
+
+gulp.task('server',function(){
+    return connect().use(serve(__dirname)).listen(8080).on('listening',function(){
+        console.log('Server at 8080');
+    })
+});
 
 gulp.task('watch', function () {
     gulp.watch('app/css/*.css', 'styles');
     gulp.watch('app/js/*.js', 'scripts');
     gulp.watch('app/img/*', 'images');
-})
+});
 
 
-gulp.task('default', gulp.parallel('images', 'scripts', 'styles','watch'));
+gulp.task('default', gulp.parallel('images', 'scripts', 'styles','server','watch'));
